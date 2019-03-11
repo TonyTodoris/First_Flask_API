325157aefe4420c03d49ecc16730fb85d62f5171
Antonis <antonistodoris@gmail.com>
2019-03-11 13:18:11 +0100

    Created REST API get stores

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 stores = [
@@ -18,8 +18,6 @@ def home():
   return "Hello, world!"
 
 
-app.run(port=5000)
-
 # POST - used to receive data
 # GET - used to send data back only
 
@@ -27,19 +25,25 @@ app.run(port=5000)
 @app.route('/store', methods=['POST'])
 def create_store():
   pass
+
 # GET /store/<string:name>
 @app.route('/store/<string:name>')
 def get_store(name):
   pass
+
 # GET /store
 @app.route('/store')
 def get_stores():
-  pass
+  return jsonify({'stores': stores})
+
 # POST /store/<string:name>/item {name: price:}
-@app.route('store/<string:name>/item', methods=['POST'])
+@app.route('/store/<string:name>/item', methods=['POST'])
 def create_item_in_store(name):
   pass
+
 # GET /store/<string:name>/item
 @app.route('/store/<string:name>/item')
 def get_item_in_store(name):
   pass
+
+app.run(port=5000)
